3d56353fffb5d51a7d6ae9e628ab01fe37c43c28
lucamanh <lucaph03@gmail.com>
2025-10-25 14:43:23 -0700

    web pages for landing and feed pages

---
+++ app/src/Pages/Landing.jsx
@@ -0,0 +1,15 @@
+import styles from './styles/Landing.module.css'
+
+function Landing() {
+    return(
+    <>
+        
+        <body>
+            <h1 className={styles.textWhite}>Hello, World!</h1>
+        </body>
+    </>
+    )    
+
+}
+
+export default Landing;

1b8ca8ae5dceae2a6b52f9a5fe87418bfc79faf1
lucamanh <lucaph03@gmail.com>
2025-10-25 23:39:20 -0700

    with landing page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Landing from './Pages/Landing.jsx'
 import './App.css'
 
@@ -9,6 +7,7 @@ function App() {
 
   return (
     <>
+      <title>ARithmetic</title>
       <Landing />
     </>
   )
--- app/src/Pages/Landing.jsx
+++ app/src/Pages/Landing.jsx
@@ -1,15 +1,29 @@
-import styles from './styles/Landing.module.css'
+import {Link} from 'react-router-dom';
+import styles from './styles/Landing.module.css';
+import shadow from '../components/shadow.png';
+import noShadow from '../components/no shadow.png';
 
 function Landing() {
     return(
     <>
-        
-        <body>
-            <h1 className={styles.textWhite}>Hello, World!</h1>
-        </body>
+        <br/><br/><br/>
+
+        <div className={styles.title}>
+            <span className={styles.bigFont}>AR</span>
+            <span style={{fontSize: '30px'}}>ithmetic</span>
+            <Link to ="/other" className={styles.imageHover}>
+                <img src={shadow} alt = 'shadow' className = {styles.normal} />
+                <img src={noShadow} alt = 'no shadow' className = {styles.hover} />
+            </Link>
+        </div>
+
+        <div>
+            <p>
+                So we made an app that's basically photomath but marginally better.
+            </p>
+        </div>
     </>
     )    
-
 }
 
 export default Landing;
